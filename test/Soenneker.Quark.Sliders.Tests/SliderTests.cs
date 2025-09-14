0af503877049eff2329bb9011d15f63ee53527e6
Jake Soenneker <jake@soenneker.com>
2025-09-14 11:38:16 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Sliders.Tests/SliderTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Sliders.Tests;
+
+[Collection("Collection")]
+public sealed class SliderTests : FixturedUnitTest
+{
+    public SliderTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
